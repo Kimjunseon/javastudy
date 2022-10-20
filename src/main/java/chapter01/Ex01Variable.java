9dfbdb3872c94ec38975dc863eddd7dc543cbf1c
Kimjunseon <junseon0722@gmail.com>
2022-10-20 10:59:21 +0900

    java

---
+++ src/main/java/chapter01/Ex01Variable.java
@@ -0,0 +1,16 @@
+package chapter01;
+
+public class Ex01Variable {
+
+	public static void main(String[] args) {
+		int i = 1000;
+		String s = "Heollo World";
+		// String은 객체지만 자료형 변수로 사용됨
+		
+		
+		System.out.println(i);
+		System.out.println(s);
+
+	}
+
+}
