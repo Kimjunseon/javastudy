b901a2d1c4c0d1c65f884a74251c6ef804a1b252
Kimjunseon <junseon0722@gmail.com>
2022-10-20 11:38:07 +0900

    java

--- src/main/java/chapter01/Ex01Variable.java
+++ src/main/java/chapter01/Ex01Variable.java
@@ -4,13 +4,20 @@ public class Ex01Variable {
 
 	public static void main(String[] args) {
 		int i = 1000;
-		String s = "Heollo World";
+		String s = "Hello World";
 		// String은 객체지만 자료형 변수로 사용됨
-		
 		
 		System.out.println(i);
 		System.out.println(s);
-
+				
+		System.out.println("변수 변경");
+		i = 2000;
+		s = "Hello Again";
+		
+		System.out.println(i);
+		System.out.println(s);
+		// 원래라면 String은 변경 시 원래 있던 값에 접근을 하지 못해 가비지 값이 되지만 자바에서는 청소됨
+		
 	}
 
 }
