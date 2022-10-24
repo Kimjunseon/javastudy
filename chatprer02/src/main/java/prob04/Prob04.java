a5f5e9c8ca7ee1031ed2ffc5962fac7bef884785
Kimjunseon <junseon0722@gmail.com>
2022-10-24 22:45:37 +0900

    문자열 역순

--- chatprer02/src/main/java/prob04/Prob04.java
+++ chatprer02/src/main/java/prob04/Prob04.java
@@ -1,24 +1,23 @@
-//package prob04;
-//public class Prob04 {
-//
-//	public static void main(String[] args) {
-//		char[] c1 = reverse("Hello World");
-//		printCharArray( c1 );
-//		
-//		char[] c2 = reverse("Java Programming!");
-//		printCharArray( c2 );
-//		
-//		
-//		
-//	}
-//	
-//	public static char[] reverse(String str) {
-//		
-//		return str;
-//	}
-//
-//	public static void printCharArray(char[] array){
-//		/* 코드를 완성합니다 */
-//		System.out.println( array );
-//	}
-//}
+package prob04;
+public class Prob04 {
+
+	public static void main(String[] args) {
+		char[] c1 = reverse("Hello World");
+		printCharArray( c1 );
+
+		char[] c2 = reverse("Java Programming!");
+		printCharArray( c2 );
+		
+	}	
+	
+	public static char[] reverse(String str) {
+		char[] charArrary = str.toCharArray();
+		return charArrary;
+	}
+
+	public static void printCharArray(char[] array){
+		for(int i = array.length - 1; i >= 0 ; i--) {
+			System.out.print( array[i] );
+		}System.out.println("");
+	}
+}
