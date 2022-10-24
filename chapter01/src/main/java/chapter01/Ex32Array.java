dde40c1c1f05d4befdb11d6179c4fc59d4d41307
Kimjunseon <junseon0722@gmail.com>
2022-10-24 09:52:30 +0900

    기본프로그래밍 사용 방법

---
+++ chapter01/src/main/java/chapter01/Ex32Array.java
@@ -0,0 +1,37 @@
+package chapter01;
+
+public class Ex32Array {
+
+	public static void main(String[] args) {
+		// 선언 시 크기를 지정할 수 없다.
+		// int[10] a1; 은 생성이 안됨
+		
+		// 강추
+		int[] a2 = new int[5];
+		
+		// 비추
+		int a3[]= new int[5];
+		
+		System.out.println(a2.length);
+		System.out.println(a3.length);
+		
+		// 초기화 1 {}이 new 역할을 대신 함
+		int[] a4 = {0, 1, 2, 3, 4};
+		System.out.println(a4.length);
+		
+		// 초기화 2
+		int [] a5 =  new int[5];
+		a5[0] = 0;
+		a5[1] = 1;
+		a5[2] = 2;
+		a5[3] = 3;
+		a5[4] = 4;
+		System.out.println(a5.length);
+		
+		// 초기화 3
+		int[] a6 = new int[]{0, 1, 2, 3, 4};
+		System.out.println(a6.length);
+		
+	}
+
+}
--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -19,4 +19,3 @@ public class Prob5 {
 		}
 		}
 }
-//
