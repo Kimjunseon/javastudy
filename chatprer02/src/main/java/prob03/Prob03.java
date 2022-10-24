7d88878a891dc84c86822617fe3734833c2cf903
Kimjunseon <junseon0722@gmail.com>
2022-10-24 17:54:33 +0900

    pratice2

--- chatprer02/src/main/java/prob03/Prob03.java
+++ chatprer02/src/main/java/prob03/Prob03.java
@@ -1,17 +1,32 @@
-//package prob03;
-//
-//public class Prob03 {
-//	public static void main(String args[]) {
-//		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
-//
-//		// 원래 배열 원소 출력
-//		printCharArray(c);
-//
-//		// 공백 문자 바꾸기
-//		replaceSpace(c);
-//
-//		// 수정된 배열 원소 출력
-//		printCharArray(c);
-//	}
-//
-//}
+package prob03;
+
+public class Prob03 {
+	public static void main(String args[]) {
+		char[] charArrary = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
+
+		// 원래 배열 원소 출력
+		printCharArray(charArrary);
+
+		// 공백 만들기
+		replaceSpace(charArrary);
+		
+		// 수정된 배열 원소 출력
+		printCharArray(charArrary);
+	}
+	
+	public static void printCharArray(char[] cs) {
+		for(int i = 0; i < cs.length; i++) {
+			System.out.print(cs[i]);
+		} System.out.println("");
+	}
+
+	 public static void replaceSpace(char[] cs) {
+		 for(int i = 0; i < cs.length; i++) {
+			if(cs[i] == ' '){
+				cs[i] = ',';
+			}
+			}
+		}
+
+	}
+
