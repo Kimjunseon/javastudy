84ecc6caea759ce9668e298206463535f57738eb
Kimjunseon <junseon0722@gmail.com>
2022-10-26 23:34:27 +0900

    진행중,,

--- practice03/src/main/java/prob02/Goods.java
+++ practice03/src/main/java/prob02/Goods.java
@@ -1,6 +1,37 @@
 package prob02;
 
 public class Goods {
+	private String drinkName;
+	private int drinkPrice;
+	private int drinkCount;
+
 	public void showInfo() {
+		System.out.println(drinkName + "(가격:" +
+						   drinkPrice + "원)이 " +
+					       drinkCount + "개 입고 되었습니다.)");
+	}
+
+	public String getDrinkName() {
+		return drinkName;
+	}
+
+	public void setDrinkName(String drinkName) {
+		this.drinkName = drinkName;
+	}
+
+	public int getDrinkPrice() {
+		return drinkPrice;
+	}
+
+	public void setDrinkPrice(int drinkPrice) {
+		this.drinkPrice = drinkPrice;
+	}
+
+	public int getDrinkCount() {
+		return drinkCount;
+	}
+
+	public void setDrinkCount(int drinkCount) {
+		this.drinkCount = drinkCount;
 	}
 }
--- practice03/src/main/java/prob02/GoodsApp.java
+++ practice03/src/main/java/prob02/GoodsApp.java
@@ -8,7 +8,6 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
 		Goods[] goods = new Goods[COUNT_GOODS];
 
 		// 상품 입력
@@ -22,7 +21,7 @@ public class GoodsApp {
 		// int count = 
 		
 		// 상품 출력
-		
+		goods.showInfo();
 		// 자원정리
 		scanner.close();
 	}
