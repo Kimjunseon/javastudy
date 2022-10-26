dbf1c3fe850abf5a0fd759a1f3ea5e0cfca80764
Kimjunseon <junseon0722@gmail.com>
2022-10-26 17:56:05 +0900

    member

--- practice03/src/main/java/prob01/Member.java
+++ practice03/src/main/java/prob01/Member.java
@@ -1,4 +1,33 @@
 package prob01;
 
 public class Member {
+	private String memberID;
+	private String memberName;
+	private int memberPoint;
+	
+	public void info() {
+		System.out.println("회원[아이디: "+ memberID +
+						   ", 이름:" + memberName +
+						   ", 점수: " + memberPoint + "]");
+		
+	}
+	
+	public String getMemberID() {
+		return memberID;
+	}
+	public void setMemberID(String memberID) {
+		this.memberID = memberID;
+	}
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	public int getMemberPoint() {
+		return memberPoint;
+	}
+	public void setMemberPoint(int memberPoint) {
+		this.memberPoint = memberPoint;
+	}
 }
---
+++ practice03/src/main/java/prob01/memberClass.java
@@ -0,0 +1,14 @@
+package prob01;
+
+public class memberClass {
+
+	public static void main(String[] args) {
+		Member member = new Member();
+		member.setMemberID("junseon0722");
+		member.setMemberName("김준선");
+		member.setMemberPoint(325);
+		member.info();
+		
+	}
+
+}
