67f8db27934862838de1baa23a1070db91a83594
Kimjunseon <junseon0722@gmail.com>
2022-10-26 13:48:55 +0900

    d

--- chapter03/src/main/java/paint/Point.java
+++ chapter03/src/main/java/paint/Point.java
@@ -2,6 +2,11 @@ package paint;
 public class Point {
 	private int x;
 	private int y;
+	
+	public Point(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
 	public int getX() {
 		return x;
 	}
@@ -14,9 +19,19 @@ public class Point {
 	public void setY(int y) {
 		this.y = y;
 	}
-	public void show() {
-		System.out.println("점(X=" + x + ", y=" + y + ")를 그렸습니다.");
-		
+	public void show(boolean visible) {
+		if(visible) {
+			show();
+		} else {
+			System.out.println("점(X=" + x + ", y=" + y + ")를 지웠습니다.");
+		}
 	}
+	
+	public void show() {
+			System.out.println("점(X=" + x + ", y=" + y + ")를 그렸습니다.");
+	}
+//	public void disappear() {
+//		System.out.println("점(X=" + x + ", y=" + y + ")를 지웠습니다.");
+//	}
 
 }
