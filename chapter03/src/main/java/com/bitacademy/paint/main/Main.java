58d6188b133d8aa89548749872f8b5c46582cda6
Kimjunseon <junseon0722@gmail.com>
2022-10-27 14:10:52 +0900

    package

--- chapter03/src/main/java/com/bitacademy/paint/main/Main.java
+++ chapter03/src/main/java/com/bitacademy/paint/main/Main.java
@@ -1,4 +1,13 @@
-package paint;
+package com.bitacademy.paint.main;
+
+import com.bitacademy.paint.i.Drawable;
+import com.bitacademy.paint.point.ColorPoint;
+import com.bitacademy.paint.point.Point;
+import com.bitacademy.paint.shape.Circle;
+import com.bitacademy.paint.shape.Rectangle;
+import com.bitacademy.paint.shape.Shape;
+import com.bitacademy.paint.shape.Triangle;
+import com.bitacademy.paint.text.GraphicText;
 
 public class Main {
 
@@ -10,6 +19,8 @@ public class Main {
 		draw(pt1);
 		// pt.disappear();
 		pt1.show(false);
+		
+		
 		
 		Point pt2 = new ColorPoint(100, 200, "red");
 //		pt2.setX(100);
@@ -37,6 +48,16 @@ public class Main {
 		GraphicText gt = new GraphicText("Hello World");
 		draw(gt);
 		
+		// instanceof test (상속이 되어 있는지 안돼있는지 확인)
+		System.out.println(circle instanceof Object);
+		System.out.println(circle instanceof Shape);
+		System.out.println(circle instanceof Circle);
+		// 오류: class는 hierachy 상위와 하위만 instanceof 연산자를 사용할 수 있다.
+		// System.out.println(circle instanceof Triangle); 오류코드
+		
+		// 하지만, interface는 hierachy와 상관없이 instanceof 연산자를 사용할 수 있다.
+		System.out.println(circle instanceof Drawable);
+		System.out.println(circle instanceof Runnable);
 	}	
 	
 
