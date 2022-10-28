9c5d666d2703cf97816675829497c8e824f5091f
Kimjunseon <junseon0722@gmail.com>
2022-10-28 09:57:43 +0900

    pratice

--- practice01/src/main/java/prob2/Prob2.java
+++ practice01/src/main/java/prob2/Prob2.java
@@ -1,15 +1,28 @@
+//package prob2;
+//
+//public class Prob2 {
+//	public static void main(String[] args) {
+//		for(int i = 1; i < 9; i++) {
+//			System.out.print(i);
+//			System.out.print(" ");
+//			for(int j = 1; j < 10; j++) {
+//				System.out.print(i+j);
+//				System.out.print(" ");
+//			}System.out.print("\n");
+//		}
+//			
+//	}
+//}
+
 package prob2;
 
 public class Prob2 {
 	public static void main(String[] args) {
-		for(int i = 1; i < 9; i++) {
-			System.out.print(i);
-			System.out.print(" ");
-			for(int j = 1; j < 10; j++) {
-				System.out.print(i+j);
-				System.out.print(" ");
-			}System.out.print("\n");
+		for(int i = 1; i <= 9; i++) {
+			for(int j = 1; j < i + 10; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.print("\n");
 		}
-			
 	}
 }
