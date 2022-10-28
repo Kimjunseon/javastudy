1af91a6388ff1d40a9f8181679eb14844902d40c
Kimjunseon <junseon0722@gmail.com>
2022-10-28 15:37:10 +0900

    API

--- chapter04/src/main/java/chapter04/ObjectTest01.java
+++ chapter04/src/main/java/chapter04/ObjectTest01.java
@@ -5,11 +5,13 @@ public class ObjectTest01 {
 	public static void main(String[] args) {
 		Point p = new Point(10, 20);
 		
-		// Class kclass  = p.getClass();
+		// Class klass  = p.getClass();
 		System.out.println(p.getClass()); // reflection 기술
-		System.out.println(p.hashCode());
-		System.out.println(p.toString());
-	
+		System.out.println(p.hashCode()); // address 기반의 해싱 값
+										  // address x
+ 										  // reference x
+		System.out.println(p.toString()); // getClass() + "@" + hashCode()
+		System.out.println(p);
 	}
 
 }
