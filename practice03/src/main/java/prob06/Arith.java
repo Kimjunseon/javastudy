3470454373c8d9af879ee797f1bb51d077634f1e
Kimjunseon <junseon0722@gmail.com>
2022-11-01 21:34:01 +0900

    계산기

--- practice03/src/main/java/prob06/Arith.java
+++ practice03/src/main/java/prob06/Arith.java
@@ -1,5 +1,13 @@
 package prob06;
 
-public class Arith {
+public abstract class Arith {
+	public int a;
+	public int b;
+	
+	public void setValue(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	
 
 }
--- practice03/src/main/java/prob06/Div.java
+++ practice03/src/main/java/prob06/Div.java
@@ -1,13 +1,6 @@
 package prob06;
 //수정해야함 추상클래스 중복제거하라는것 가타 리펙토링하기 아리스 클래스추가 칼쿨앱도 수정이 필요 더하기 빼기 나누기 곱하기 단에서 수정이 많이 된다고함
-public class Div {
-	private int a;
-	private int b;
-	public void setValue(int a, int b) {
-		this.a = a;
-		this.b = b;
-	}
-	
+public class Div extends Arith{
 	public int calculate() {
 		return a / b;
 	}
