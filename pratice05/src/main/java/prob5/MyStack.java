5f6863648b00bc923223f8659260082a067c7ae8
Kimjunseon <junseon0722@gmail.com>
2022-11-02 00:11:37 +0900

    pra05

--- pratice05/src/main/java/prob5/MyStack.java
+++ pratice05/src/main/java/prob5/MyStack.java
@@ -4,6 +4,16 @@ public class MyStack {
 	private String[] buffer;
 	
 	public MyStack(int size) {
-		
+	}
+
+	public void push(String string) {
+	}
+
+	public boolean isEmpty() {
+		return false;
+	}
+
+	public String pop() {
+		return null;
 	}
 }
